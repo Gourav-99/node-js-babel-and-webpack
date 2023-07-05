129f80b478a6ca266a25dfeac00e0885a230e1c2
Gourav <goupg1999@gmail.com>
2023-07-05 15:11:04 +0530

    Revert "server-code-todo-app"
    This reverts commit 4ac68aa5f2aa249e0ae7ba780b90cab02bcd6635.

--- server/src/utils/db.utils.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL, {});
-    console.warn("Connected to DB");
-  } catch (error) {
-    console.error(error.message);
-  }
-};
